9152f13288726fb8f3996fe38099bac79f995992
brunoNabarrete <brunonabarrete@hotmail.com>
2023-08-28 14:32:08 -0300

    finalizando o projeto

--- octo-test-typescript/src/components/SearchLocation/index.tsx
+++ octo-test-typescript/src/components/SearchLocation/index.tsx
@@ -1,45 +1,38 @@
 import React, { useState } from "react";
-import axios from "axios";
 
 interface Address {
   cep: string;
   logradouro: string;
   bairro: string;
-  cidade: string;
+  localidade: string;
   uf: string;
 }
 
 export default function SearchAddress() {
-  const [street, setStreet] = useState<string>("");
+  const [uf, setUf] = useState<string>("");
+  const [cidade, setCidade] = useState<string>("");
+  const [rua, setRua] = useState<string>("");
   const [addressOptions, setAddressOptions] = useState<Address[]>([]);
   const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
   const [error, setError] = useState<string>('');
 
-  async function getAddressOptions(streetName: string) {
+  async function getAddressOptions() {
     setError('');
-    if (streetName.length < 3) {
-      setError('No mínimo 3 caracteres');
-      setAddressOptions([]);
-      return;
-    }
 
-    if (/\d/.test(streetName)) {
-      setError('Não é permitido números');
+    if (!uf || !cidade || !rua) {
+      setError('Preencha todos os campos');
       setAddressOptions([]);
       return;
     }
 
     try {
-      const formattedStreetName = streetName.replace(/\s/g, '-');
-      const response = await fetch(`http://cep.la/${encodeURIComponent(formattedStreetName)}`, {
-        headers: {
-          "Accept": "application/json"
-        }
-      });
+      const response = await fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${rua}/json/`);
       const responseData = await response.json();
-      setAddressOptions(responseData);
+      console.log(responseData)
+      setAddressOptions([responseData][0]);
+      console.log(addressOptions)
     } catch (error) {
-      console.error(`Erro ao obter opções para o nome da rua:`, error);
+      console.error(`Erro ao obter opções para o endereço:`, error);
     }
   }
 
@@ -48,33 +41,54 @@ export default function SearchAddress() {
     setAddressOptions([]);
   }
 
+
   return (
     <div className="w-11/12 lg:w-3/5 p-4 lg:p-20 bg-white mt-8 lg:mt-14 mb-8 lg:mb-14 rounded-xl">
-      <h2 className="text-lg font-semibold mb-4">Consulta de Endereço:</h2>
-      <div className="flex-wrap">
-        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-          </div>
-          <input
-            type="text"
-            id="default-search"
-            value={street}
-            onChange={(e) => setStreet(e.target.value)}
-            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Digite o endereço"
-            required
-          />
-          <button
-            type="button"
-            onClick={() => getAddressOptions(street)}
-            className="text-white absolute right-2.5 bottom-2.5 font-bold opacity-75 text-sm lg:text-base text-white font-medium rounded-md text-center bg-roxo-principal hover:bg-roxo-secundario hover:text-white rounded-lg transition duration-300 ease-out hover:ease-in px-2 py-1"
-          >
-            Search
-          </button>
+    <h2 className="text-lg font-semibold mb-4">Consulta de Endereço:</h2>
+    <div className="flex-wrap">
+      <div className="mb-4">
+        <label htmlFor="uf" className="text-sm font-medium text-gray-900">UF</label>
+        <input
+          type="text"
+          id="uf"
+          value={uf}
+          onChange={(e) => setUf(e.target.value)}
+          className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="UF"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="cidade" className="text-sm font-medium text-gray-900">Cidade</label>
+        <input
+          type="text"
+          id="cidade"
+          value={cidade}
+          onChange={(e) => setCidade(e.target.value)}
+          className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Cidade"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="rua" className="text-sm font-medium text-gray-900">Rua</label>
+        <input
+          type="text"
+          id="rua"
+          value={rua}
+          onChange={(e) => setRua(e.target.value)}
+          className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Rua"
+          required
+        />
+      </div>
+      <button
+        type="button"
+        onClick={getAddressOptions}
+        className="text-white font-bold opacity-75 text-sm lg:text-base font-medium rounded-md text-center bg-roxo-principal hover:bg-roxo-secundario hover:text-white rounded-lg transition duration-300 ease-out hover:ease-in px-4 py-2"
+      >
+        Search
+      </button>
         </div>
         <div>
           {error && (
@@ -90,7 +104,7 @@ export default function SearchAddress() {
               <h5 className="mb-2 text-xl tracking-tight text-gray-900"><b>CEP:</b> {address.cep}</h5>
               <h5 className="mb-2 text-xl tracking-tight text-gray-900 "><b>Logradouro:</b> {address.logradouro}</h5>
               <h5 className="mb-2 text-xl tracking-tight text-gray-900 "><b>Bairro:</b> {address.bairro}</h5>
-              <h5 className="mb-2 text-xl tracking-tight text-gray-900 "><b>Cidade:</b> {address.cidade}</h5>
+              <h5 className="mb-2 text-xl tracking-tight text-gray-900 "><b>Cidade:</b> {cidade}</h5>
               <h5 className="mb-2 text-xl tracking-tight text-gray-900 "><b>Estado:</b> {address.uf}</h5>
               <button
                 onClick={() => handleAddressSelection(address)}
@@ -101,17 +115,17 @@ export default function SearchAddress() {
             </div>
           ))}
         </div>
-      </div>
-      {selectedAddress && (
+        <div>
+        {selectedAddress && (
         <div className="border-t text-black mt-4 pt-4">
           <h3 className="text-lg font-semibold text-black mb-2">Endereço Selecionado:</h3>
           <p>CEP: {selectedAddress.cep}</p>
           <p>Logradouro: {selectedAddress.logradouro}</p>
           <p>Bairro: {selectedAddress.bairro}</p>
-          <p>Cidade: {selectedAddress.cidade}</p>
           <p>Estado: {selectedAddress.uf}</p>
         </div>
       )}
-    </div>
-  );
+      </div>
+      </div>
+  )
 }
